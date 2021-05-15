e09444c7fbaf5bcac334047f804811a6f471dd3e
ahaangupta <80193060+ahaangupta@users.noreply.github.com>
2021-05-15 18:23:27 +1000

    Add files via upload

---
+++ Project-C28-template-main/sling.js
@@ -0,0 +1,14 @@
+class sling{
+    constructor(pointA,pointB){
+        var options={
+            pointA: pointA,
+            pointB: pointB
+        }
+        this.pointB = stone
+        this.sling = Constraint.create(options)
+        world.add(world.this.sling)
+    }
+    fly(){
+        this.sling.bodyA = null
+    }
+}
